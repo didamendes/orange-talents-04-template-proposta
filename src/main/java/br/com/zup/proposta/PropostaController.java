296bbaf27c4313a021de3367583ad62efa74f5de
Diogo <didamendes@hotmail.com>
2021-05-06 15:09:21 -0300

    Não pode haver proposta

--- src/main/java/br/com/zup/proposta/PropostaController.java
+++ src/main/java/br/com/zup/proposta/PropostaController.java
@@ -1,5 +1,7 @@
 package br.com.zup.proposta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
 @RestController
 @RequestMapping("/propostas")
 public class PropostaController {
@@ -18,9 +22,19 @@ public class PropostaController {
     @Autowired
     private PropostaRepository propostaRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(PropostaController.class);
+
     @PostMapping
     public ResponseEntity<PropostaResponse> salvar(@Valid @RequestBody PropostaRequest propostaRequest) {
+        logger.info(" Proposta do cliente= {} com salario= {} recebida ", propostaRequest.getNome(), propostaRequest.getSalario());
         Proposta proposta = propostaRequest.converter();
+        boolean exists = propostaRepository.existsByDocumento(proposta.getDocumento());
+
+        if (exists) {
+            logger.error("Proposta existente");
+            return ResponseEntity.status(UNPROCESSABLE_ENTITY).build();
+        }
+
         propostaRepository.save(proposta);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(proposta.getId()).toUri();
--- src/main/java/br/com/zup/proposta/PropostaRepository.java
+++ src/main/java/br/com/zup/proposta/PropostaRepository.java
@@ -3,4 +3,6 @@ package br.com.zup.proposta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+
+    boolean existsByDocumento(String documento);
 }
